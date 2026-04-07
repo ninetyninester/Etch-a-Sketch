91c079d68234bf8822f40cd8877675c7e2e170d3
ninetyninester <icaro.alencar99@gmail.com>
2026-04-06 21:48:25 -0300

    create the grid

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,52 @@
+let container = document.querySelector("#container");
+
+let mouseTrigger;
+
+document.addEventListener("mousedown", () => {
+    mouseTrigger = true;
+})
+
+document.addEventListener("mouseup", () => {
+    mouseTrigger = false;
+})
+
+for (let i = 1; i <= 16; i++){
+
+    let line = document.createElement("div")
+    line.classList.add("line");
+    container.appendChild(line);
+
+    for (let i = 1; i <= 16; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        square.style.backgroundColor = "blue";
+        })
+
+    square.addEventListener("mouseover", (e) => {
+        if(mouseTrigger){
+        e.preventDefault();
+        square.style.backgroundColor = "blue";
+        }
+    })
+
+    // square.addEventListener("mousedown", (e) => {
+    //     if(e.button == 2){
+    //     e.preventDefault();
+    //     square.style.backgroundColor = "white";
+    //     }
+    // })
+
+    // square.addEventListener("mouseover", (e) => {
+    //     if(mouseTrigger && e.button == 2){
+    //     e.preventDefault();
+    //     square.style.backgroundColor = "white";
+    //     }
+    // })
+
+    line.appendChild(square);
+    }
+}
+
